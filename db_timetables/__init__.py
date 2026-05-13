08bb2605df7572e7f05d30d2f029d031982ca10f
mbbrueckner <mikabenbrueckner@icloud.com>
2026-05-13 12:19:45 +0200

    feat: add `__init__.py` for db_timetables module with exports

---
+++ db_timetables/__init__.py
@@ -0,0 +1,47 @@
+from .client import TimetablesClient
+from .models import (
+    Station,
+    Timetable,
+    TimetableStop,
+    TrainLine,
+    ArrivalDeparture,
+    Message,
+    DistributorMessage,
+    Connection,
+    HistoricDelay,
+    HistoricPlatformChange,
+    MessageType,
+    EventStatus,
+    ConnectionStatus,
+    DistributorType,
+    DelaySource,
+)
+from .exceptions import (
+    DBApiError,
+    AuthenticationError,
+    NotFoundError,
+    RateLimitError,
+)
+
+__all__ = [
+    "TimetablesClient",
+    "Station",
+    "Timetable",
+    "TimetableStop",
+    "TrainLine",
+    "ArrivalDeparture",
+    "Message",
+    "DistributorMessage",
+    "Connection",
+    "HistoricDelay",
+    "HistoricPlatformChange",
+    "MessageType",
+    "EventStatus",
+    "ConnectionStatus",
+    "DistributorType",
+    "DelaySource",
+    "DBApiError",
+    "AuthenticationError",
+    "NotFoundError",
+    "RateLimitError",
+]
